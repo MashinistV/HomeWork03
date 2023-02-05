b286387f839de6eb52528b29886736246be9e9d5
Sergey <mashinistv@gmail.com>
2023-02-05 13:22:40 +0300

    Task 19 complited

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите целое пятизначное число: ");
+int.TryParse(Console.ReadLine(), out int number);
+while (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Вы ввели неверную цифру");
+    Console.Write("Введите целое пятизначное число: ");
+    int.TryParse(Console.ReadLine(), out number);
+}
+if (((number % 100000) / 10000) == (number % 10) && ((number % 10000) / 1000) == ((number % 100) / 10))
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
